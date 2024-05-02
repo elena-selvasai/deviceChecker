e69e13720b8aabaa5b2aea9ae8bd975de657d71e
Elena Yumi Yi <ymyi@selvas.com>
2024-05-02 11:12:06 +0900

    getUserMedia 추가

--- devieChecker.js
+++ devieChecker.js
@@ -15,6 +15,9 @@ if (system.platform === undefined) {
 
 setWindowSize();
 setResult();
+initAudio((result) => {
+    document.querySelector(".user_media_box").innerHTML = "getUserMedia : " + result;
+})
 window.addEventListener('resize', () => setTimeout(() => setWindowSize(), 300));
 
 function handleDeviceChange(event) {
@@ -33,7 +36,7 @@ navigator.mediaDevices.enumerateDevices()
         console.log('Available media devices:', devices);
     })
     .catch(function (error) {
-        setMediaInfo('Error enumerating devices:'+ JSON.stringify(error));
+        setMediaInfo('Error enumerating devices:' + JSON.stringify(error));
         console.error('Error enumerating devices:', error);
     });
 
@@ -99,4 +102,53 @@ function setWindowSize() {
 
 function setMediaInfo(text) {
     document.querySelector(".media_box").innerHTML = text;
+}
+
+function initAudio(onCallback) {
+    if (navigator.mediaDevices === undefined) {
+        navigator.mediaDevices = {};
+    }
+
+    // Some browsers partially implement mediaDevices. We can't just assign an object
+    // with getUserMedia as it would overwrite existing properties.
+    if (navigator.mediaDevices.getUserMedia === undefined) {
+        navigator.mediaDevices.getUserMedia = function (constraints) {
+            // First get ahold of the legacy getUserMedia, if present
+            let getUserMedia =
+                navigator.getUserMedia ||
+                navigator.webkitGetUserMedia ||
+                navigator.mozGetUserMedia ||
+                navigator.msGetUserMedia ||
+                navigator.oGetUserMedia;
+            if (!navigator.cancelAnimationFrame)
+                navigator.cancelAnimationFrame =
+                    navigator.webkitCancelAnimationFrame ||
+                    navigator.mozCancelAnimationFrame;
+            if (!navigator.requestAnimationFrame)
+                navigator.requestAnimationFrame =
+                    navigator.webkitRequestAnimationFrame ||
+                    navigator.mozRequestAnimationFrame;
+
+            // Some browsers just don't implement it - return a rejected promise with an error
+            // to keep a consistent interface
+            if (!getUserMedia) {
+                return Promise.reject(
+                    new Error("getUserMedia is not implemented in this browser")
+                );
+            }
+
+            // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+            return new Promise(function (resolve, reject) {
+                getUserMedia.call(navigator, constraints, resolve, reject);
+            });
+        };
+    }
+    navigator.mediaDevices
+        .getUserMedia({mediaSource: true, audio: true})
+        .then(function (stream) {
+            if (onCallback) onCallback(true);
+        })
+        .catch(function (e) {
+            if (onCallback) onCallback(false);
+        });
 }
